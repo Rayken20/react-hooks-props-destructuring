b1d61bbb9034ea53e497e6bde7e590bb2b595566
Rachael <rachael92njoki@gmail.com>
2024-02-02 23:59:04 +0300

    complete

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -4,11 +4,13 @@ function MovieCard({
   title,
   posterSrc = "https://m.media-amazon.com/images/M/MV5BOTJjNzczMTUtNzc5MC00ODk0LWEwYjgtNzdiOTEyZmQxNzhmXkEyXkFqcGdeQXVyNzMzMjU5NDY@._V1_UY268_CR1,0,182,268_AL_.jpg",
   genres,
+  posterURL,
 }) {
   return (
     <div className="movie-card">
       <img src={posterSrc} alt={title} />
       <h2>{title}</h2>
+      <h6>{posterURL}</h6>
       <small>{genres.join(", ")}</small>
     </div>
   );
